78ce0a76169616de77ca5e73f63d9c6e45df771e
Christos Tsiligiannis <67221281+xristostsilis@users.noreply.github.com>
2024-05-27 13:35:56 +0300

    Ensure correct router setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Events from './pages/Events';
-import PastEvents from './pages/PastEvents';  // Import the PastEvents page
+import PastEvents from './pages/PastEvents';
 import Contact from './pages/Contact';
 import Purchase from './pages/Purchase';
 import Footer from './components/Footer';
@@ -16,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/past-events" element={<PastEvents />} />  {/* Add the route for PastEvents */}
+        <Route path="/past-events" element={<PastEvents />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/purchase" element={<Purchase />} />
       </Routes>
