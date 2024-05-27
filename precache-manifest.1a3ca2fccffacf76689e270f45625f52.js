f6e502d9940fe2e508273b58e9f9f00e01157147
Christos Tsiligiannis <67221281+xristostsilis@users.noreply.github.com>
2024-05-27 13:49:17 +0300

    Updates

---
+++ precache-manifest.1a3ca2fccffacf76689e270f45625f52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f6a39611a3392da067c06ae6b2621ca",
+    "url": "/artinpatra npm run build/index.html"
+  },
+  {
+    "revision": "855ca7d800b5efafacef",
+    "url": "/artinpatra npm run build/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "8ae0ea7f8deac9003940",
+    "url": "/artinpatra npm run build/static/css/main.9b1544ea.chunk.css"
+  },
+  {
+    "revision": "855ca7d800b5efafacef",
+    "url": "/artinpatra npm run build/static/js/2.9a225de7.chunk.js"
+  },
+  {
+    "revision": "4af0879d6cc718af40a926fab3642ba5",
+    "url": "/artinpatra npm run build/static/js/2.9a225de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8979c4d44ca433e610ea",
+    "url": "/artinpatra npm run build/static/js/3.49638cf4.chunk.js"
+  },
+  {
+    "revision": "8ae0ea7f8deac9003940",
+    "url": "/artinpatra npm run build/static/js/main.dd20412c.chunk.js"
+  },
+  {
+    "revision": "66f3a8c8343effbc82de",
+    "url": "/artinpatra npm run build/static/js/runtime-main.a53e7de4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5cae264215b1dd4cf2546d9a5337bbe.js"
+  "/artinpatra npm run build/precache-manifest.1a3ca2fccffacf76689e270f45625f52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/artinpatra npm run build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
