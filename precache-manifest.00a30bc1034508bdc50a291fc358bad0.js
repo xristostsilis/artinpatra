e4f2d34612a106e1685b7819da5f9a22b5be1cb0
Christos Tsiligiannis <67221281+xristostsilis@users.noreply.github.com>
2024-05-27 13:24:39 +0300

    Updates

---
+++ precache-manifest.00a30bc1034508bdc50a291fc358bad0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beffb5170d345c950512e70f3e5a45d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "855ca7d800b5efafacef",
+    "url": "/static/css/2.fe62fa80.chunk.css"
+  },
+  {
+    "revision": "f9bee4284275c2864b62",
+    "url": "/static/css/main.9b1544ea.chunk.css"
+  },
+  {
+    "revision": "855ca7d800b5efafacef",
+    "url": "/static/js/2.9a225de7.chunk.js"
+  },
+  {
+    "revision": "4af0879d6cc718af40a926fab3642ba5",
+    "url": "/static/js/2.9a225de7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8979c4d44ca433e610ea",
+    "url": "/static/js/3.49638cf4.chunk.js"
+  },
+  {
+    "revision": "f9bee4284275c2864b62",
+    "url": "/static/js/main.4ab92043.chunk.js"
+  },
+  {
+    "revision": "006667517e74849f5dd4",
+    "url": "/static/js/runtime-main.fdabf17a.js"
+  }
+]);
